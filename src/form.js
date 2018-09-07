e8b377e6db02262fc6ecabcf5f06f46f35d5f02a
Rodrixzzz <rodcejas@gmail.com>
2018-09-07 01:00:26 -0300

    OK!

--- src/form.js
+++ src/form.js
@@ -1,37 +1,74 @@
 $(document).ready(formHandler);
 
 function formHandler() {
-  $('form').on('submit', validateFields);
+  $('form').on('submit', formValidate);
+  $('input[type=button]').on('mousedown', cambiarType);
+  $('input[type=button]').on('mouseup', cambiarType);
 }
 
-function validateFields($event) {
-  var $name = $('#name');
-  var $password = $('#password');
-  var $sendButton = $('div.buttons').find('input[type=submit]');
-  var formError = false;
-
-  if ($name.val() === '') {
-    $name.addClass('error');
-    formError = true;
-  } else {
-    $name.removeClass('error');
-  }
-  if ($password.val() === '') {
-    $password.addClass('error');
-    formError = true;
-  } else {
-    $password.removeClass('error');
-  }
+function formValidate($event) {
+  var formError = validateFields();
   if (formError) {
+    ErrorHandler(formError);
     $event.preventDefault();
-    animar($sendButton);
+    animarButton();
+  } else {
+    ErrorHandler(formError);
+    validarPassword($event);
   }
 }
 
+function validateFields() {
+  var $inputs = $('.input-JS');
+  var invalido = false;
+  $inputs.each(function(index, element) {
+    if ($(element).val() === '') {
+      invalido = true;
+    }
+  });
+  return invalido;
+}
 
-function animar($elemento) {
-  $elemento.addClass('animated hinge slow-2s ');
+function ErrorHandler(condicion) {
+  var $inputs = $('.input-JS');
+  $inputs.each(function(index, element) {
+    if (condicion) {
+      $(element).addClass('error');
+    } else {
+      $(element).removeClass('error');
+    }
+  });
+}
+
+function validarPassword($event) {
+  var $inputs = $('input[type=password]');
+  if ($($inputs[0]).val() !== $($inputs[1]).val()) {
+    ErrorHandler(true);
+    animarButton();
+    $event.preventDefault();
+  }
+}
+
+function animarButton() {
+  var $sendButton = $('div.buttons').find('input[type=submit]');
+  var imgIncorrecto = $('.incorrecto');
+  $sendButton.addClass('animated hinge slow');
+  imgIncorrecto.removeClass('hidden');
+  imgIncorrecto.addClass('animated rotateIn slow');
   setTimeout(function() {
-    $elemento.removeClass('animated hinge slow-2s ');
+    $sendButton.removeClass('hinge');
+    $sendButton.addClass('bounceInUp');
+    imgIncorrecto.removeClass('animated rotateIn slow');
   }, 2500);
+}
+function cambiarType() {
+  var confimar = $('#conf-Password');
+  var password = $('#password');
+  if (confimar[0].type === 'text') {
+    confimar[0].type = 'password';
+    password[0].type = 'password';
+  } else {
+    confimar[0].type = 'text';
+    password[0].type = 'text';
+  }
 }
